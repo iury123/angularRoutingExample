75b7d6b62b72b3e24560cc48ee8c1a9d09a14136
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2019-02-10 17:44:15 -0200

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeroesModule } from './heroes/heroes.module';
 
@@ -12,13 +12,14 @@ import { HeroesModule } from './heroes/heroes.module';
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    // os módulos especificos vem primeiro.
+    HeroesModule,
     AppRoutingModule,
-    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
